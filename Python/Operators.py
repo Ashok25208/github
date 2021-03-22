c9f46422b3116426f65ccaa6052ac28c72068e81
Ashok25208 <ashok.somasundaram20@gmail.com>
2021-03-22 23:41:03 +0800

    Operators upto arithematic

--- Python/Operators.py
+++ Python/Operators.py
@@ -1,4 +1,29 @@
 #Datatyoes and Operators
 name = 'ashok'
-type(name)== str
+print(type(name)==str)
+print(len(name)==4)
+print(isinstance(name,str))
 print(name)
+#casting
+age = int("20")
+print(age)
+float_value = 0.6575
+intFractiontoInt = int(float_value)
+print(intFractiontoInt)
+#Operators
+##assignment Operators
+score = 200
+print(score)
+##Arithematic Operators
+score = 200
+score += 50
+print("After adding "+ str(score))
+score -= 150
+print("After subtracting "+ str(score))
+score *= 2
+print(score)
+score /= 2
+print(score)
+score %= 3
+print(score)
+###Comparison Operators
